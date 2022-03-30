0ab7b998763301d693f35fa7f7d2186df1a3fe4c
Howard0223 <74956078+Howard0223@users.noreply.github.com>
2022-03-30 11:17:47 +0800

    80以及8001向集群的服務註冊中心成功註冊

--- cloud2022/cloud-eureka-server7002/src/main/java/tw/com/myproject/springcloud/EurekaMain7002.java
+++ cloud2022/cloud-eureka-server7002/src/main/java/tw/com/myproject/springcloud/EurekaMain7002.java
@@ -7,8 +7,8 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 @SpringBootApplication
 @EnableEurekaServer
 public class EurekaMain7002 {
-public static void main(String[] args) throws Exception {
-	SpringApplication.run(EurekaMain7002.class, args);
-}
+	public static void main(String[] args) throws Exception {
+		SpringApplication.run(EurekaMain7002.class, args);
+	}
 
 }
