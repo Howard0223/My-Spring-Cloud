ed995e1954dd7a4075757cd7da10b7897148b03b
Howard0223 <74956078+Howard0223@users.noreply.github.com>
2022-04-07 17:15:22 +0800

    更換負載均衡規則

--- cloud2022/cloud-consumer-order80/src/main/java/tw/com/myproject/springcloud/OrderMain80.java
+++ cloud2022/cloud-consumer-order80/src/main/java/tw/com/myproject/springcloud/OrderMain80.java
@@ -3,9 +3,13 @@ package tw.com.myproject.springcloud;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
+
+import tw.com.myproject.springcloud.myrule.MySelfRule;
 
 @SpringBootApplication
 @EnableEurekaClient
+@RibbonClient(name = "cloud-payment-service", configuration = MySelfRule.class)
 public class OrderMain80 {
 	public static void main(String[] args) {
 		SpringApplication.run(OrderMain80.class, args);
